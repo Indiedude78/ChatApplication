7d249ee890dc776c2924c18ffac74eb4ab79966b
Indiedude78 <saj39@njit.edu>
2020-09-15 15:51:06 -0400

    Java Data

---
+++ MyWorkspace/src/Homework.java
@@ -0,0 +1,21 @@
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String userName = "Linus Torvalds";
+		// 2. Output the name to the console
+		System.out.println("Name:  " + userName);
+		// 3. Create a variable that holds an age (as a whole number)
+		int userAge = 50;
+		// 4. Output the age to the console
+		System.out.println("Age:   " + userAge);
+		// 5. Create a variable that holds a decimal
+		float userRandomVar = 6.3f;
+		// 6. Output the variable
+		System.out.println("Floating point Variable:  " + userRandomVar);
+	}
+
+}
